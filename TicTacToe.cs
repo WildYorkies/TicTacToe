c1ab9b4710ba17695016a4c3e13e2bbc683c2bf5
Cale <cale.short@gmail.com>
2014-12-12 00:23:30 -0500

    Update TicTacToe.cs

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -2,43 +2,47 @@ using System;
 					
 public class Program
 {
+	
 	static string gameBoard = "1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9";
 	
 	public static void Main()
 	{
 		
-		bool loop = true;
 		bool keepPlaying = false;
-		
-		Console.WriteLine(gameBoard);
 		
 		do
 		{
+			Console.WriteLine(gameBoard);
 			Console.WriteLine("Type a number to place an X");
 			string userChoice = Console.ReadLine();
+			
+			GameBoardRefresh(userChoice); // Rewrite this to make gameBoard an object & have "Refresh" be a behavior?
 		
+			// Checks to see if there are still numbers in the board
+			// Why won't this work correctly?
 			for (int i=1; i==9; i++)
 			{
 				if (gameBoard.Contains(Convert.ToString(i)))
 				{
 					keepPlaying = true;
+					break;
+				}
+				else 
+				{
+					keepPlaying = false;
 				}
 			}
-			
-			GameBoardRefresh(userChoice);
-			
 		} while (keepPlaying == false);
 		
-	
+		Console.WriteLine("Good job!");
 	}
 	
 	public static void GameBoardRefresh(string userIn)
 	{
 		if (gameBoard.Contains(userIn))
 		{
-			gameBoard = gameBoard.Replace(userIn, "X"); //[index] = "X";
-		}
-		
-		Console.WriteLine(gameBoard);
+			gameBoard = gameBoard.Replace(userIn, "X");
+		}		
 	}
+	
 }
